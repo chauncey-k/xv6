5e61827b93d7cfcbad83441e4a229ff60191d9fd
Chauncey <1073695360@qq.com>
2026-03-12 19:59:51 +0800

    Lab: system calls from original repo

--- kernel/proc.c
+++ kernel/proc.c
@@ -292,6 +292,9 @@ fork(void)
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
+  // copy trace mask from parent to child
+  np->trace_mask = p->trace_mask;
+
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
@@ -653,4 +656,20 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+uint64
+nproc(void)
+{
+  struct proc *p;
+  uint64 n = 0;
+
+  for(p = proc; p < &proc[NPROC]; p++){
+    acquire(&p->lock);
+    if(p->state != UNUSED)
+      n++;
+    release(&p->lock);
+  }
+
+  return n;
 }
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,8 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "syscall.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -94,4 +96,32 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+// trace system call, set the trace mask of the current process
+uint64
+sys_trace(void)
+{
+  int mask;
+  if(argint(0, &mask) < 0)  // get the trace mask from the first argument
+    return -1;
+  myproc()->trace_mask = mask;  // set the trace mask of the current process
+  return 0;
+}
+
+uint64
+sys_sysinfo(void)
+{
+  struct sysinfo info;
+  uint64 addr;
+
+  argaddr(0, &addr);  // get the address of the sysinfo struct from the first argument
+
+  info.freemem = freemem();
+  info.nproc = nproc();
+
+  if(copyout(myproc()->pagetable, addr, (char *)&info, sizeof(info)) < 0)
+    return -1;
+
+  return 0;
 }
